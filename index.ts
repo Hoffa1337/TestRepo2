8eef30b2cf0744dd66ac82ae6ccbfa2285b5a59f
Hoffa <robin.hofstrom@gmail.com>
2017-11-17 10:35:31 +0100

    Initial payload. Added index.ts file

---
+++ index.ts
@@ -0,0 +1,10 @@
+module com.teknikgymnasiet.programmering {
+    export class Lektion2 {
+        constructor() {
+            console.log("HELLO WORLD !!");
+        }
+    }
+}
+
+
+let instans : any = new com.teknikgymnasiet.programmering.Lektion2();
